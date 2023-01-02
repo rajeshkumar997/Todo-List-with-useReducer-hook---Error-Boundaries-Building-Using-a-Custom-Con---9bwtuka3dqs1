72e93e4c65ef3842cd14d0fb7837ccf40b4de908
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-02 23:36:45 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,6 +1,18 @@
 const todoReducer = (state, action) => {
     switch (action.type) {
+    import React from "react";
+import "../styles/App.css";
 
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <div className="logo">Todo List</div>
+      <button className="theme">Theme</button>
+    </div>
+  );
+};
+
+export default Navbar;
     }
 }
 
