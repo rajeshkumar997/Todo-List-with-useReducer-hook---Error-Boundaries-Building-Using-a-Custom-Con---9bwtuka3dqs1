377a8060481dd524c4618dbcc908601e9f960b69
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-02 23:47:26 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/App.css";
-
-const Navbar = () => {
+const Todo = ({ title, id, handleDelete }) => {
   return (
-    <div className="Navbar">
-      <div className="logo">Todo List</div>
-      <button className="theme">Theme</button>
+    <div className="todo">
+      <div className="todo-title">{title}</div>
+      <button className="todo-delete" onClick={() => handleDelete(id)}>
+        Delete
+      </button>
     </div>
   );
 };
 
-export default Navbar;
+export { Todo };
